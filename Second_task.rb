7be7fd0d85a23bf7340aef90b9f9647bd14a42e0
Danchek <denisdotsenko7@gmail.com>
2016-09-28 15:45:26 +0300

    Add files via upload

--- Second_task.rb
+++ Second_task.rb
@@ -1,6 +1,6 @@
 str = "Abbabb123"
 simvoly = "Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk  Ll Mm Nn 
-Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz"
+			Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz"
 massiv=simvoly.split(" ")
 massiv.each do |i|
 	if (str.count i) > 0
