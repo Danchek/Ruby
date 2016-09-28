3fd53f814e503ef484d937f58bfd9b8c10334906
Danchek <denisdotsenko7@gmail.com>
2016-09-28 15:42:17 +0300

    Add files via upload

---
+++ Second_task.rb
@@ -0,0 +1,10 @@
+str = "Abbabb123"
+simvoly = "Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk  Ll Mm Nn 
+Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz"
+massiv=simvoly.split(" ")
+massiv.each do |i|
+	if (str.count i) > 0
+		otvet = [i[0..0],(str.count i)].join(" ")
+		p otvet
+	end
+end
